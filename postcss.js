ffd5d40f1fe4452b336aadb70b83409b917e9660
Matt Gaunt-Seo <matt@gaunt.dev>
2023-02-12 17:07:50 -0800

    Add first build

---
+++ postcss.js
@@ -0,0 +1,38 @@
+import { readFile, writeFile } from 'node:fs/promises';
+import * as path from 'path';
+
+import glob from 'glob';
+import postcssPresetEnv from 'postcss-preset-env';
+import postcssGlobalData from '@csstools/postcss-global-data';
+import postcss from 'postcss';
+import cssnano from 'cssnano';
+
+const SRC_DIR = 'project-repo/themes';
+const BUILD_DIR = 'project-repo/public';
+
+async function start() {
+	const plugins = [
+		postcssGlobalData({
+			files: glob.sync(path.join(SRC_DIR, '**', 'variables', '*.css')),
+		}),
+		postcssPresetEnv({
+			preserve: true,
+		}),
+		cssnano(),
+	];
+	const processor = postcss(plugins);
+
+	const cssFiles = glob.sync(path.join(BUILD_DIR, '**', '*.css'));
+	for (const c of cssFiles) {
+		if (path.basename(c).indexOf('_') == 0) {
+			// Skip files starting with underscore
+			continue;
+		}
+
+		const css = await readFile(c);
+		const result = await processor.process(css, { from: c });
+		await writeFile(c, result.css);
+	}
+}
+
+start();
